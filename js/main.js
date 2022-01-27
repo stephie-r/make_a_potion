8b5b0fdafdcf6c6d52878a15180804bf3d5abdb0
stephie <reynolds.stephie@gmail.com>
2022-01-26 22:28:38 -0300

    redo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const botonClick = document.querySelector('#boton-click');
+const input = document.querySelector('input-botones');
+
+botonClick.addEventListener('click', ()=>{
+  input.value = '';
+});
+
+//for (let i = 0; i < localStorage.length; i++) {
+//    console.log(localStorage.getItem(localStorage.key(i)));
+//  }
+
+//const colores = [
+//    'red',
+//    'gray',
+//]
+
