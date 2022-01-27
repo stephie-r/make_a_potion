c67d07533ac64c2b3f2a46ad758c54d44c3543ce
stephie <reynolds.stephie@gmail.com>
2022-01-26 23:51:04 -0300

    toggle/ldmode

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
-const botonClick = document.querySelector('#boton-click');
+/*const botonClick = document.querySelector('#boton-click');
 const input = document.querySelector('input-botones');
 
 botonClick.addEventListener('click', ()=>{
   input.value = '';
 });
 
-//for (let i = 0; i < localStorage.length; i++) {
-//    console.log(localStorage.getItem(localStorage.key(i)));
-//  }
+for (let i = 0; i < localStorage.length; i++) {
+        console.log(localStorage.getItem(localStorage.key(i)));
+  }
 
-//const colores = [
-//    'red',
-//    'gray',
-//]
+const colores = [
+    'red',
+    'gray',
+]*/
 
+document.getElementById("light_dark").addEventListener("click", darkMode);
+
+function darkMode() {
+  alert ("Hello World!");
+}
