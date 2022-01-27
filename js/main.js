7af3733061815b6c8868d8fdc34ac6fa71ce50db
stephie <reynolds.stephie@gmail.com>
2022-01-27 19:45:08 -0300

    inner.HTML

--- js/main.js
+++ js/main.js
@@ -1,30 +1,21 @@
-/*const botonClick = document.querySelector('#boton-click');
-const input = document.querySelector('input-botones');
 
-botonClick.addEventListener('click', ()=>{
-  input.value = '';
-});
-
-for (let i = 0; i < localStorage.length; i++) {
-        console.log(localStorage.getItem(localStorage.key(i)));
-  }
-
-const colores = [
-    'red',
-    'gray',
-]*/
-/*
-let toggle = document.getElementById('toggle');
-
-toggle.onclick = function (){
-    toggle.classList.toggle(active);
-};*/
 const body = document.querySelector('body');
 const toggleButton = document.querySelector("#toggle");
 const switchMode = document.querySelector(".switch_mode");
 toggleButton.addEventListener("click", darkMode);
-function darkMode() {
+function darkMode() { //SWITCH EVENT DARK LIGHT MODE
     switchMode.classList.toggle("active");
     body.classList.toggle("active");
     toggleButton.classList.toggle("active");
+}
+
+const buttonCta = document.querySelector("#cta");
+const mainP = document.querySelector("#p1");
+buttonCta.addEventListener("click",startClass);
+function startClass() {//START CLASS
+  element.innerHTML = "New Heading"
+  let answer = prompt(
+    "Hi and welcome to your first potion class! Shall we get started? yes / no"
+  ).toLowerCase();
+
 }
